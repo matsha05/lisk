8b513304334abe5ed365fa8b3a0e49879d6410ae
Lucas Isasmendi <lucas@lightcurve.io>
2017-11-02 17:06:17 +0100

    Replace fillPool with processPool

--- modules/delegates.js
+++ modules/delegates.js
@@ -595,7 +595,7 @@ Delegates.prototype.onRoundChanged = function (data) {
 
 /**
  * Loads delegates.
- * @implements module:transactions#Transactions~fillPool
+ * @implements module:transactions#Transactions~processPool
  */
 Delegates.prototype.onBlockchainReady = function () {
 	__private.loaded = true;
@@ -611,7 +611,7 @@ Delegates.prototype.onBlockchainReady = function () {
 
 			async.waterfall([
 				__private.forge,
-				modules.transactions.fillPool
+				modules.transactions.processPool
 			], function () {
 				return setImmediate(cb);
 			});
