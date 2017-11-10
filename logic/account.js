e4f7ae4b447a401276f4e3ef1bc9f119ee192d5b
isabello <isabella@lightcurve.io>
2017-11-10 12:52:55 +0100

    Fix dbSave for votes to save pkey instead of bytes

--- logic/account.js
+++ logic/account.js
@@ -139,6 +139,16 @@ function Account (db, schema, logger, cb) {
 			expression: '("rank")::bigint'
 		},
 		{
+			name: 'delegates',
+			type: 'Text',
+			filter: {
+				type: 'array',
+				uniqueItems: true
+			},
+			conv: Array,
+			expression: '(SELECT ARRAY_AGG(ENCODE("delegate_public_key", \'hex\')) FROM votes_details WHERE "voter_address" = a."address")'
+		},
+		{
 			name: 'multisignatures',
 			type: 'Text',
 			filter: {
